df869cf7c6fbdd92c08f4b737564a8ced29b3cf9
Thiago Goncalves <thiagosg80@gmail.com>
2023-07-11 17:40:09 -0300

    Excluidos os arredondamentos

--- analise/service/lucro_presumido.py
+++ analise/service/lucro_presumido.py
@@ -15,38 +15,37 @@ class LucroPresumidoService:
         lucro_presumido.presuncao_csll = self.__get_presuncao(PercentualLucroPresumido.PRESUNCAO_CSLL.value,
                                                               faturamento_periodo)
 
-        irpj = round(lucro_presumido.presuncao_irpj.montante * PercentualLucroPresumido.IRPJ.value, 2)
+        irpj = lucro_presumido.presuncao_irpj.montante * PercentualLucroPresumido.IRPJ.value
         lucro_presumido.irpj = irpj
         adicional_irpj = self.__get_adicional_irpj(lucro_presumido.presuncao_irpj.montante)
         lucro_presumido.adicional_irpj = adicional_irpj
-        csll = round(lucro_presumido.presuncao_csll.montante * PercentualLucroPresumido.CSLL.value, 2)
+        csll = lucro_presumido.presuncao_csll.montante * PercentualLucroPresumido.CSLL.value
         lucro_presumido.csll = csll
-        pis = round(faturamento_periodo * PercentualLucroPresumido.PIS.value, 2)
+        pis = faturamento_periodo * PercentualLucroPresumido.PIS.value
         lucro_presumido.pis = pis
-        cofins = round(faturamento_periodo * PercentualLucroPresumido.COFINS.value, 2)
+        cofins = faturamento_periodo * PercentualLucroPresumido.COFINS.value
         lucro_presumido.cofins = cofins
-        icms = round(faturamento_periodo * PercentualLucroPresumido.ICMS.value - valor_medio_credito_icms, 2)
+        icms = faturamento_periodo * PercentualLucroPresumido.ICMS.value - valor_medio_credito_icms
         lucro_presumido.icms = icms
         inss_salarios = salarios_valor * PercentualLucroPresumido.INSS_SALARIOS.value
         inss_pro_labore= pro_labore_valor * PercentualLucroPresumido.INSS_PRO_LABORE.value
-        inss = round(inss_salarios + inss_pro_labore, 2)
+        inss = inss_salarios + inss_pro_labore
         lucro_presumido.inss = inss
         lucro_presumido.valor_medio_credito_icms = valor_medio_credito_icms
-        carga_tributaria_anual = round(irpj + adicional_irpj + csll + pis + cofins + icms + inss, 2)
+        carga_tributaria_anual = irpj + adicional_irpj + csll + pis + cofins + icms + inss
         lucro_presumido.carga_tributaria_anual = carga_tributaria_anual
-        lucro_presumido.percentual_dos_tributos = round(carga_tributaria_anual / faturamento_periodo, 4)
+        lucro_presumido.percentual_dos_tributos = carga_tributaria_anual / faturamento_periodo
 
         return lucro_presumido
 
     def __get_presuncao(self, aliquota: float, faturamento: float) -> Presuncao:
         presuncao = Presuncao()
         presuncao.aliquota = aliquota
-        presuncao.montante = round(faturamento * aliquota, 2)
+        presuncao.montante = faturamento * aliquota
 
         return presuncao
 
     def __get_adicional_irpj(self, reference: float) -> float:
         corte = 60 * 1000
 
-        return round((reference - corte) * PercentualLucroPresumido.ADICIONAL_IRPJ.value, 2) if reference > corte \
-            else 0
+        return (reference - corte) * PercentualLucroPresumido.ADICIONAL_IRPJ.value if reference > corte else 0
--- analise/service/simples_nacional.py
+++ analise/service/simples_nacional.py
@@ -6,14 +6,14 @@ class SimplesNacionalService:
     def get(self, faturamento_periodo: float, salarios_valor: float, pro_labore_valor: float) -> SimplesNacional:
         simples_nacional = SimplesNacional()
         simples_nacional.aliquota = PercentualSimplesNacional.ALIQUOTA.value
-        a_pagar = round(faturamento_periodo * PercentualSimplesNacional.ALIQUOTA.value, 2)
+        a_pagar = faturamento_periodo * PercentualSimplesNacional.ALIQUOTA.value
         simples_nacional.a_pagar_no_periodo = a_pagar
         inss_salarios = salarios_valor * PercentualSimplesNacional.SALARIOS.value
         inss_pro_labore = pro_labore_valor * PercentualSimplesNacional.PRO_LABORE.value
-        inss = round(inss_salarios + inss_pro_labore, 2)
+        inss = inss_salarios + inss_pro_labore
         simples_nacional.inss = inss
-        carga = round(a_pagar + inss, 2)
+        carga = a_pagar + inss
         simples_nacional.carga_tributaria_anual = carga
-        simples_nacional.percentual_dos_tributos = round(carga / faturamento_periodo, 4)
+        simples_nacional.percentual_dos_tributos = carga / faturamento_periodo
 
         return simples_nacional
